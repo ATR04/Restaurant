38c2724ffd39eb22e2e04e77ebede55c74ce316d
ATR <athibanthangaraja05@gmail.com>
2023-08-16 22:59:06 +0530

    Menu card ui done and db create for menu

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { MenuCardComponent } from './menu-card/menu-card.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    MenuCardComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    MenuCardComponent
   ]
 })
 export class ComponentsModule { }
